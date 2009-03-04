fb3ed73d1362ea2bd27b0ff8ae8f7c76afcf642a
Dizan Vasquez <dichodaemon@gmail.com>
2009-03-04 21:35:19 +0000

    * Updated examples to make use of RemoteRegistry. * Added test configurations in cfg.

---
+++ rotor-example/src/rotor-example/receiver.cpp
@@ -0,0 +1,21 @@
+#include "Point.h"
+#include <rotor/RemoteRegistry.h>
+#include <rotor/Serialization.h>
+#include <iostream>
+
+using namespace Rotor;
+using namespace std;
+
+//------------------------------------------------------------------------------
+
+int main( int argc, char * argv[] ) {
+  RemoteRegistry registry( argv[0] );
+  registry.registerType( ROTOR_DEFINITION_STRING( elrob_point_6d_t ) );
+  registry.subscribeToMessageType( "elrob_smart_pos_message", ROTOR_DEFINITION_STRING( elrob_smart_pos_message ) );
+
+  while ( true ) {
+    Message m = registry.receiveMessage();
+    cout << m.data->toString() << endl;
+    delete m.data;
+  }
+}
---
+++ rotor-example/src/rotor-example/sender.cpp
@@ -0,0 +1,45 @@
+#include "Point.h"
+#include <rotor/RemoteRegistry.h>
+#include <rotor/Thread.h>
+#include <rotor/Time.h>
+
+using namespace std;
+using namespace Rotor;
+
+//------------------------------------------------------------------------------
+
+int main( int argc, char * argv[] ) {
+  RemoteRegistry registry( argv[0] );
+  registry.registerType( ROTOR_DEFINITION_STRING( elrob_point_6d_t ) );
+  registry.registerMessageType( "elrob_smart_pos_message", ROTOR_DEFINITION_STRING( elrob_smart_pos_message ) );
+
+  {
+    // First way
+    Structure pose = registry.newStructure( "elrob_smart_pos_message" );
+    pose["pose"]["x"] = 12;
+    pose["pose"]["y"] = 12;
+    pose["host"] = "fotoinforsic";
+    registry.sendStructure( "elrob_smart_pos_message", pose );
+  }
+  {
+    // Second way
+    elrob_smart_pos_message p;
+    p.host = "forolifo";
+    
+    // Structure pose is an 'alias' of p
+    Structure pose = registry.newStructure( "elrob_smart_pos_message", &p ); 
+    
+    int value = 0;
+    while ( true ) {
+      Thread::sleep( 20 );
+      value += 1;
+      p.pose.x = value;
+      p.pose.y = value * 2;
+      p.velocity = value * 1;
+      p.steering_angle = value * 2;
+      p.timestamp = seconds();
+      registry.sendStructure( "elrob_smart_pos_message", pose );
+    }
+  }
+}
+ 
