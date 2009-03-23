1f0f06fc73716a41800b14e1f1d9b6d2521722e1
Dizan Vasquez <dichodaemon@gmail.com>
2009-03-23 13:49:55 +0000

    * Limited acceleration on joy2smart.

--- rotor-drivers/python/rotor/joy2smart/joy2smart.py
+++ rotor-drivers/python/rotor/joy2smart/joy2smart.py
@@ -36,17 +36,47 @@ registry.subscribeToMessage( "JOYSTICK" )
 registry.registerMessageType( "elrob_smart_steering_message", steeringDefinition )
 command = rotorc.Structure( "elrob_smart_steering_message", None, registry )
 
-oldTime = 0
+velocity = 0
+oldTime = rotorc.seconds()
 
 while True:
   message = registry.receiveMessage()
-  time    = rotorc.seconds()
   joystick = message.data
   command.velocity      = -joystick.axes[1] * options.getDouble( robotName, "maxSpeed" )
   command.steeringangle = -joystick.axes[0] * options.getDouble( robotName, "maxSteeringAngle" )
   command.timestamp     = rotorc.microseconds();
   command.host          = "ventisca"
+
+  time    = rotorc.seconds()
+  deltaT  = time - oldTime
+  deltaV  = command.velocity - velocity
+  acceleration    = deltaV / deltaT
+  maxAcceleration = options.getDouble( robotName, "maxAcceleration" )
+  maxDeceleration = options.getDouble( robotName, "maxDeceleration" )
+  if velocity == 0:
+    if acceleration > maxAcceleration:
+      rotorc.Logger.debug( "Starting forward" )
+      command.velocity = maxAcceleration * deltaT
+    elif acceleration < -maxAcceleration:
+      rotorc.Logger.debug( "Starting backwards" )
+      command.velocity = -maxAcceleration * deltaT
+  elif velocity > 0:
+    if acceleration > maxAcceleration:
+      rotorc.Logger.debug( "Accelerating forward" )
+      command.velocity = velocity + maxAcceleration * deltaT
+    elif acceleration < -maxDeceleration:
+      rotorc.Logger.debug( "Braking forward" )
+      command.velocity = velocity - maxDeceleration * deltaT
+  elif velocity < 0:
+    if acceleration < -maxAcceleration:
+      rotorc.Logger.debug( "Accelerating backwards" )
+      command.velocity = velocity - maxAcceleration * deltaT
+    elif acceleration > maxDeceleration:
+      rotorc.Logger.debug( "Braking backwards" )
+      command.velocity = velocity + maxDeceleration * deltaT
+      
+  velocity = command.velocity
+
+
   registry.sendStructure( "elrob_smart_steering_message", command )
-  sys.stdout.write( "\r%f\t%f \t%f" % ( command.velocity, command.steeringangle, time - oldTime )  )
-  sys.stdout.flush()
   oldTime = time 
