0572496e4a606feba074a8d4bcb4da1a35633bc5
Dizan Vasquez <dichodaemon@gmail.com>
2009-04-22 14:28:49 +0000

    * Updated receiver example.

--- rotor-example/src/rotor-example/receiver.cpp
+++ rotor-example/src/rotor-example/receiver.cpp
@@ -11,7 +11,8 @@ using namespace std;
 int main( int argc, char * argv[] ) {
   RemoteRegistry registry( argv[1] );
   registry.registerType( ROTOR_DEFINITION_STRING( elrob_point_6d_t ) );
-  registry.subscribeToMessageType( "elrob_smart_pos_message", ROTOR_DEFINITION_STRING( elrob_smart_pos_message ) );
+  registry.registerMessageType( "elrob_smart_pos_message", ROTOR_DEFINITION_STRING( elrob_smart_pos_message ) );
+  registry.subscribeToMessage( "elrob_smart_pos_message", true );
 
   while ( true ) {
     Message m = registry.receiveMessage();
