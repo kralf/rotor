d9c7da9e56a3862439c55343dadfd165c694405c
Dizan Vasquez <dichodaemon@gmail.com>
2009-08-17 15:34:02 +0000

    * Updated example to latest version of rotor.

--- rotor-example/src/rotor-example/sender.cpp
+++ rotor-example/src/rotor-example/sender.cpp
@@ -14,36 +14,26 @@ int main( int argc, char * argv[] ) {
   registry.registerType( ROTOR_DEFINITION_STRING( elrob_point_6d_t ) );
   registry.registerMessageType( "elrob_smart_pos_message", ROTOR_DEFINITION_STRING( elrob_smart_pos_message ) );
 
-  {
-    // First way
-    Structure pose = registry.newStructure( "elrob_smart_pos_message" );
-    pose["pose"]["x"] = 12;
-    pose["pose"]["y"] = 12;
-    pose["host"] = "fotoinforsic";
+  // Second way
+  elrob_smart_pos_message p;
+  p.host = "fotoinforsic";
+  
+  // Structure pose is an 'alias' of p
+  Structure pose = registry.newStructure( "elrob_smart_pos_message", &p ); 
+  
+  
+  int value = 0;
+  while ( true ) {
+    Logger::spam( pose.toString() );
+    Thread::sleep( 0.1 );
+    value += 1;
+    p.pose.x = value;
+    p.pose.y = value * 2;
+    p.velocity = value * 1;
+    p.steering_angle = value * 2;
+    p.timestamp = seconds();
+    Logger::debug( pose.toString() );
     registry.sendStructure( "elrob_smart_pos_message", pose );
-  }
-  {
-    // Second way
-    elrob_smart_pos_message p;
-    p.host = "fotoinforsic";
-    
-    // Structure pose is an 'alias' of p
-    Structure pose = registry.newStructure( "elrob_smart_pos_message", &p ); 
-    
-    
-    int value = 0;
-    while ( true ) {
-      Logger::spam( pose.toString() );
-      Thread::sleep( 0.1 );
-      value += 1;
-      p.pose.x = value;
-      p.pose.y = value * 2;
-      p.velocity = value * 1;
-      p.steering_angle = value * 2;
-      p.timestamp = seconds();
-      Logger::debug( pose.toString() );
-      registry.sendStructure( "elrob_smart_pos_message", pose );
-    }
   }
 }
  
