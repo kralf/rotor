ff56f3838e7d6d8a42c23cd5f49274763b6b787e
Ralf Kaestner <ralf.kaestner@gmail.com>
2009-11-09 18:23:12 +0000

    Added new-lines to prevent compiler warnings in Rotor CARMEN backports

--- rotor-carmen/src/_carmen/CarmenHandler.h
+++ rotor-carmen/src/_carmen/CarmenHandler.h
@@ -5,7 +5,7 @@
 #include <rotor/Message.h>
 #include <rotor/Registry.h>
 #include <rotor/Queue.h>
-#include <carmen/ipc.h>
+#include <libipc/ipc.h>
 #include <queue>
 #include <map>
 
@@ -21,17 +21,17 @@ class CarmenHandler
 {
 public:
   typedef std::pair< Message, MSG_INSTANCE> QueryInfo;
-  
+
   CarmenHandler( CarmenRegistry & registry );
   virtual ~CarmenHandler();
-  
+
   Registry & registry();
-  
+
   void reply( const Message & message );
-  
+
   void enqueueQuery( Message & message, MSG_INSTANCE msgInstance );
   void enqueueReply( Message & message );
-  
+
   QueryInfo dequeueQuery( double timeout );
   Structure dequeueReply( double timeout );
 
@@ -39,11 +39,11 @@ public:
   static void handleMessage( MSG_INSTANCE msgInstance, void * data, void * handlerPtr );
   static void handleQuery( MSG_INSTANCE msgInstance, void * data, void * handlerPtr );
   static void handleReply( MSG_INSTANCE msgInstance, BYTE_ARRAY byteArray, void * handlerPtr );
-  
+
 private:
   typedef std::queue< MSG_INSTANCE > InstanceQueue;
   CarmenRegistry &   _registry;
-  Thread *           _dispatchThread; 
+  Thread *           _dispatchThread;
   Queue<QueryInfo >  _queryQueue;
   Queue<Message>     _replyQueue;
   InstanceQueue      _instanceQueue;
--- rotor-carmen/src/carmen/CarmenRegistry.h
+++ rotor-carmen/src/carmen/CarmenRegistry.h
@@ -19,33 +19,33 @@ class CarmenRegistry : public Registry
 {
 public:
   CarmenRegistry( const std::string & name, Options & options );
-  
+
   virtual ~CarmenRegistry();
-  
+
   virtual const std::string & name() const;
-  
+
   virtual Options & options() const;
-  
-  virtual const Type & 
+
+  virtual const Type &
   registerType( const std::string & definitionString );
-  
-  virtual const Type & 
+
+  virtual const Type &
   operator[]( const std::string & typeName ) const;
 
-  virtual void 
-  registerMessage( 
-    const std::string & messageName, 
+  virtual void
+  registerMessage(
+    const std::string & messageName,
     const std::string & typeName );
 
-  virtual void subscribeToMessage( 
+  virtual void subscribeToMessage(
     const std::string & messageName,
     bool queueOwner = false,
     size_t queueCapacity = 0,
-    QueuePolicy queuePolicy = DISCARD_OLDEST );  
+    QueuePolicy queuePolicy = DISCARD_OLDEST );
 
   virtual void subscribeToQuery( const std::string & messageName );
 
-  virtual const Type & 
+  virtual const Type &
   messageType( const std::string & messageName ) const;
 
   virtual double
@@ -55,9 +55,9 @@ public:
 
   virtual Message receiveMessage( double timeout = 0 ) throw( MessagingTimeout );
 
-  virtual Message receiveMessage( 
-    const std::string & messageName, 
-    double timeout = 0 ) 
+  virtual Message receiveMessage(
+    const std::string & messageName,
+    double timeout = 0 )
   throw( MessagingTimeout );
 
   virtual Structure query( const Message & message, double timeout = 0 ) throw( MessagingTimeout );
@@ -79,7 +79,7 @@ private:
   QueueHandler     _queueHandler;
   MessageQueue     _outputQueue;
   TimestampQueues  _timestampQueues;
-  
+
   friend class CarmenHandler;
 };
 
