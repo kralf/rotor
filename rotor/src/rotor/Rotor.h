2e775b9885c9f71c1af533b06e7316f7fc561a4a
Dizan Vasquez <dichodaemon@gmail.com>
2010-03-08 18:14:31 +0000

    Neutral: Added ROTOR_VERSION definition in Rotor.h Neutral: Updated Rotor version number to 1.1.0 Neutral: New rotor carmen also checks for standard version of ipc.

---
+++ rotor/src/rotor/Rotor.h
@@ -0,0 +1,14 @@
+#ifndef ROTOR_H
+#define ROTOR_H
+
+
+#include "Structure.h"
+#include "Exceptions.h"
+#include "RegistryMacros.h"
+#include <string>
+
+
+#define ROTOR_VERSION "1.1.0"
+
+
+#endif //ROTOR_H
